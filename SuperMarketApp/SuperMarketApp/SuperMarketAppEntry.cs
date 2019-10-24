49f04193dd9431178322aa9faf775dde42dc2b1c
Rajesh Balli <rajb4u@gmail.com>
2019-10-24 19:28:10 +0100

    * SuperMarketApp.Tests.csproj: Changed version information and added   Main entry point class.
    * SuperMarketAppEntry.cs:

---
+++ SuperMarketApp/SuperMarketApp/SuperMarketAppEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SuperMarketApp
+{
+    class SuperMarketAppEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n\nPlease run the tests for verifying the implementation.\n\n\n");
+        }
+    }
+}
